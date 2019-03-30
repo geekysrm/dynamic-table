858b452d8330f07e19e69c8c536b87c45d51853c
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2019-03-30 15:08:56 +0530

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import FirstComponent from "./components/FirstComponent";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>Hello World</p>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            {/*<Navbar />*/}
+            <Route exact path="/" component={FirstComponent} />
+
+            {/*<Footer />*/}
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class FirstComponent extends Component {
   render() {
-    return (
-      <div>
-        First component
-      </div>
-    )
+    return <div>First component</div>;
   }
 }
